851aaa81204416c8e6b082ad0191e78065c8d06c
KarinaSakova <karina.sakov@gmail.com>
2021-10-11 11:25:39 +0300

    Homework1

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+alert("Привет, это работа Саковой Карины");
+
+const myPlanet = 'Земля';
+alert(myPlanet);
+
+
+let myAge;
+myAge='24';
+alert(myAge);
+
+const mySurname='Сакова';
+const myName='Карина';
+alert(`Привет, меня зовут ${myName} ${mySurname}, мне ${myAge} года`);
+
+let myTeacher = prompt('Как тебя зовут?', 'Игорь');
+alert(`Мою работу проверяет ${myTeacher}!`);
+
+//c = 2;
+//d = 1;
+
+//a=5;
+//b=4;
+//x=6;
+//y=5;
